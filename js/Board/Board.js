05d882512f52e649e4658baa08e2e8a11e8c7428
Kbuczynski <kamil.buczynski00@interia.pl>
2020-03-24 13:11:44 +0100

    created random treasures

--- js/Board/Board.js
+++ js/Board/Board.js
@@ -1,4 +1,5 @@
-import MovingField from '../MovingField/MovingField.js'
+import MovingField from '../MovingField/MovingField.js';
+import { RandomTreasuresData } from '../Data/RandomTreasuresData.js';
 
 class Board {
     constructor(roadFields) {
@@ -21,13 +22,15 @@ class Board {
     }
 
     buildNewBoard = roadFields => {
+        const { randomNumber } = this;
+
         const randomRoadFields = roadFields.filter(el => el.dataset.static == null);
-
-
+        const treasuresRoadFields = roadFields.filter(el => el.dataset.item);
 
         const rotationOptions = ['-90', '0', '90', '180'];
 
-        let optionArray = [
+        //random road fields (34)
+        const optionArray = [
             {
                 type: 'roadCorner',
                 number: 16,
@@ -42,26 +45,38 @@ class Board {
             }
         ];
 
+        for (let i = 0; i < RandomTreasuresData.length; i++) {
+            const randomTreasure = randomNumber(RandomTreasuresData);
+            const randomField = randomNumber(randomRoadFields);
+
+            randomRoadFields[randomField].dataset.item = `../img/treasures/${RandomTreasuresData[randomTreasure].name}.png`;
+        }
+
+        treasuresRoadFields.forEach(el => el.style.backgroundImage = `url(${el.dataset.item}), url(../img/roadSplit.png)`);
+
         randomRoadFields.forEach(el => {
             const filtredOptionArray = optionArray.filter(el => el.number !== 0);
 
-            const randomIndex = Math.floor(Math.random() * filtredOptionArray.length);
+            const randomIndex = randomNumber(filtredOptionArray);
 
-            const randomRotation = Math.floor(Math.random() * rotationOptions.length);
+            const randomRotation = randomNumber(rotationOptions);
 
             const index = optionArray.map(el => el.type).indexOf(filtredOptionArray[randomIndex].type);
 
             optionArray[index].number--;
 
-            el.style.backgroundImage = `url(../img/${filtredOptionArray[randomIndex].type}.png)`;
+            if (el.dataset.item === undefined) el.style.background = `url(../img/${filtredOptionArray[randomIndex].type}.png)`;
+            else el.style.background = `url(${el.dataset.item}), url(../img/${filtredOptionArray[randomIndex].type}.png)`;
+
             el.style.transform = `rotate(${rotationOptions[randomRotation]}deg)`;
         });
 
         const lastElement = optionArray.filter(el => el.number === 1)[0].type;
 
         const movingField = new MovingField(lastElement);
-
     }
+
+    randomNumber = range => Math.floor(Math.random() * range.length);
 }
 
 export default Board;
---
+++ js/Data/RandomTreasuresData.js
@@ -0,0 +1,38 @@
+export const RandomTreasuresData = [
+    {
+        name: 'fire-sword'
+    },
+    {
+        name: 'fish'
+    },
+    {
+        name: 'gold-ax'
+    },
+    {
+        name: 'gold-helmet'
+    },
+    {
+        name: 'gold-sword'
+    },
+    {
+        name: 'gold-wand'
+    },
+    {
+        name: 'ice-sword'
+    },
+    {
+        name: 'iron-helmet'
+    },
+    {
+        name: 'necklace'
+    },
+    {
+        name: 'orange-gem'
+    },
+    {
+        name: 'red-wand'
+    },
+    {
+        name: 'ring'
+    }
+];
