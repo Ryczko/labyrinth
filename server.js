8e3ac64d41aae6affe01f812ef618e3046634fb1
unknown <konradryczko1@gmail.com>
2020-07-09 18:53:57 +0200

    bug fixed

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/room", (req, res) => {
-  rooms[req.body.room] != null && res.redirect('/');
-  
+
+  console.log('przed')
+  if (rooms[req.body.room] != null) return console.log('taka nazwa już istnieje')
+  console.log('po')
   rooms[req.body.room] = { users: {} };
   res.redirect(req.body.room);
   //wyslij ze pokoj stworzony
